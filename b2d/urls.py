c3eea0caea392c567ff5d80d52d3374b62ea43cf
Jullaphong JIAMWATTHANALOET <jullaphong03@gmail.com>
2024-09-03 22:00:30 +0700

    Add reset password function

--- b2d/urls.py
+++ b2d/urls.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.views import LogoutView
+
 from django.urls import path
-from django.contrib.auth.views import LogoutView, PasswordResetView
+
 from .views import *
 
 app_name = 'b2d'
@@ -9,5 +11,4 @@ urlpatterns = [
     path('register/', RegistrationFormView.as_view(), name='registration'),
     path('login/', B2DLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('reset-password/', PasswordResetView.as_view(), name='reset-password')
 ]
