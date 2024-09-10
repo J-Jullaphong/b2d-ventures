b9b60c990aae411b912cfdd26a271d6a5a25ec13
Jullaphong JIAMWATTHANALOET <jullaphong03@gmail.com>
2024-09-10 21:53:39 +0700

    Add check_file_exist utility

--- b2d/utils.py
+++ b2d/utils.py
@@ -26,3 +26,15 @@ def upload_file(file, key):
     except ClientError as e:
         print(f"Error uploading file to S3: {e}")
         return False
+
+
+def check_file_exist(key):
+    s3 = boto3.client('s3', aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+        region_name=settings.AWS_S3_REGION_NAME,
+    )
+    try:
+        s3.head_object(Bucket=settings.AWS_STORAGE_BUCKET_NAME, Key=key)
+        return True
+    except ClientError:
+        return False
