b55fb8baa09fd7c90bec54e89da3d7c7cf4fb7a1
Jullaphong JIAMWATTHANALOET <jullaphong03@gmail.com>
2024-10-08 22:29:36 +0700

    Add docstring comments for utilities

--- b2d/utils.py
+++ b2d/utils.py
@@ -4,6 +4,7 @@ from django.conf import settings
 
 
 def get_file(key):
+    """Retrieves the content of a file from an S3 bucket."""
     s3 = boto3.client('s3', aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
                       aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
                       region_name=settings.AWS_S3_REGION_NAME)
@@ -17,6 +18,7 @@ def get_file(key):
 
 
 def upload_file(file, key):
+    """Uploads a file to an S3 bucket."""
     s3 = boto3.client('s3', aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
                       aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
                       region_name=settings.AWS_S3_REGION_NAME)
@@ -29,10 +31,11 @@ def upload_file(file, key):
 
 
 def check_file_exist(key):
+    """Checks if a file exists in an S3 bucket."""
     s3 = boto3.client('s3', aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
-        aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
-        region_name=settings.AWS_S3_REGION_NAME,
-    )
+                      aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+                      region_name=settings.AWS_S3_REGION_NAME,
+                      )
     try:
         s3.head_object(Bucket=settings.AWS_STORAGE_BUCKET_NAME, Key=key)
         return True
