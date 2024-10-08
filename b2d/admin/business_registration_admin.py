13d1b74b478fabc64c5ddbe8e9c5d64c147ef0cc
Jullaphong JIAMWATTHANALOET <jullaphong03@gmail.com>
2024-10-08 22:19:58 +0700

    Add docstring comments for admin-related classes

--- b2d/admin/business_registration_admin.py
+++ b2d/admin/business_registration_admin.py
@@ -5,6 +5,10 @@ from ..models import Business
 
 @admin.register(Business)
 class BusinessRegistrationAdmin(admin.ModelAdmin):
+    """
+        Customizes the admin interface for the Business model, allowing for
+        business registration approvals and management.
+    """
     list_display = ('name', 'email', 'business_registration_certificate',
                     'tax_identification_number',
                     'proof_of_address', 'financial_statements',
@@ -14,15 +18,17 @@ class BusinessRegistrationAdmin(admin.ModelAdmin):
     actions = ['approve_registration']
     fieldsets = (
         (None, {'fields': (
-        'name', 'email', 'phone_number', 'business_registration_certificate',
-        'tax_identification_number',
-        'proof_of_address', 'financial_statements',
-        'ownership_documents',
-        'director_identification', 'licenses_and_permits',
-        'bank_account_details', 'is_active')}),
+            'name', 'email', 'phone_number',
+            'business_registration_certificate',
+            'tax_identification_number',
+            'proof_of_address', 'financial_statements',
+            'ownership_documents',
+            'director_identification', 'licenses_and_permits',
+            'bank_account_details', 'is_active')}),
     )
 
     def approve_registration(self, request, queryset):
+        """Custom action to approve selected business registrations."""
         for registration in queryset:
             if not registration.is_active:
                 registration.is_active = True
--- b2d/admin/investor_registration_admin.py
+++ b2d/admin/investor_registration_admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
+
 from ..models import Investor
 
 
 @admin.register(Investor)
 class InvestorRegistrationAdmin(admin.ModelAdmin):
+    """
+        Customizes the admin interface for the Investor model, enabling the
+        management and approval of investor registrations.
+    """
     list_display = ('first_name', 'last_name', 'email', 'financial_statements',
                     'is_active')
     actions = ['approve_registration']
@@ -13,12 +18,13 @@ class InvestorRegistrationAdmin(admin.ModelAdmin):
     )
 
     def approve_registration(self, request, queryset):
+        """Custom action to approve selected investor registrations."""
         for registration in queryset:
             if not registration.is_active:
-
                 registration.is_active = True
                 registration.save()
 
-                self.message_user(request, f"{registration.email} approved successfully.")
+                self.message_user(request,
+                                  f"{registration.email} approved successfully.")
 
     approve_registration.short_description = "Approve selected registrations"
