d40aee58c9bc932eb016a2056107d99d8d96d668
Pichayanon Toochinda <kewkunggt@gmail.com>
2024-10-13 21:44:00 +0700

    Fix bug in investment view

--- b2d/views/investment_view.py
+++ b2d/views/investment_view.py
@@ -12,34 +12,43 @@ class InvestmentView(FormView):
     template_name = 'b2d/transaction.html'
     form_class = InvestmentForm
 
-    def get(self, request, *args, **kwargs):
-        """Provides context data to the investment template including the current fundraising campaign."""
-        try:
-            investor = Investor.objects.get(id=request.user.id)
-        except Investor.DoesNotExist:
-            return redirect("b2d:home")
-
-        context = {
-            'fundraise': get_object_or_404(FundRaising, id=self.kwargs['fundraise_id'])
-        }
-        return render(request, self.template_name, context)
-
     def get_form_kwargs(self):
-        """Passes additional keyword arguments to the form including the current fundraising campaign."""
+        """Passes additional keyword arguments to the form, including the current fundraising campaign."""
         kwargs = super().get_form_kwargs()
         fundraise = get_object_or_404(FundRaising, id=self.kwargs['fundraise_id'])
         kwargs['fundraise'] = fundraise
         return kwargs
 
-    def form_invalid(self, form):
-        """Handles form validation failure by displaying an error message and re-rendering the form."""
-        messages.error(self.request, "There was an error with your submission.")
-        return self.render_to_response(self.get_context_data(form=form))
+    def get(self, request, *args, **kwargs):
+        """Handles the GET request and provides context data to the investment template."""
+        try:
+            investor = Investor.objects.get(id=request.user.id)
+        except Investor.DoesNotExist:
+            return redirect("b2d:home")
 
-    def form_valid(self, form):
-        """Handles form validation success by saving the investment and redirecting to the business detail page."""
-        investor = Investor.objects.get(id=self.request.user.id)
         fundraise = get_object_or_404(FundRaising, id=self.kwargs['fundraise_id'])
-        form.save(investor=investor, fundraise=fundraise)
-        messages.success(self.request, "Your investment has been submitted and is pending admin approval.")
-        return redirect(reverse('b2d:business_detail', kwargs={'pk': fundraise.business_id}))
+        form = self.get_form(self.form_class)
+
+        context = {
+            'fundraise': fundraise,
+            'form': form
+        }
+        return render(request, self.template_name, context)
+
+    def post(self, request, *args, **kwargs):
+        """Handles the POST request when the form is submitted."""
+        investor = get_object_or_404(Investor, id=self.request.user.id)
+        fundraise = get_object_or_404(FundRaising, id=self.kwargs['fundraise_id'])
+        form = self.get_form(self.form_class)
+        context = {
+            'fundraise': fundraise,
+            'form': form
+        }
+
+        if form.is_valid():
+            investment = form.save(investor=investor, fundraise=fundraise)
+            messages.success(self.request, "Your investment has been submitted and is pending admin approval.")
+            return redirect(reverse('b2d:business_detail', kwargs={'pk': fundraise.business_id}))
+
+        messages.error(self.request, "There was an error with your submission.")
+        return render(self.request, self.template_name, context)
